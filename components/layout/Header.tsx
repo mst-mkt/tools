000854a515d25f94dc98ff705a0e30c7023546df
mst-mkt <s1f102303057@iniad.org>
2024-03-09 03:58:09 +0900

    add: github link with icon into the right of the header

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,10 +1,17 @@
 import { EmojiYarn } from "../emoji/Yarn.tsx";
+import IconBrandGithubFilled from "$tabler-icons-tsx/tsx/brand-github-filled.tsx";
 
 export const Header = () => {
   return (
     <header class="flex items-center gap-x-4">
       <EmojiYarn width="32" height="32" />
-      <h1 class="text-2xl">Tools</h1>
+      <h1 class="text-2xl flex-grow flex-shrink truncate">Tools</h1>
+      <a
+        href="http://github.com/mst-mkt/tools"
+        class="hover:bg-neutral-200 transition-colors p-2 rounded-lg"
+      >
+        <IconBrandGithubFilled class="w-6 h-6" />
+      </a>
     </header>
   );
 };
