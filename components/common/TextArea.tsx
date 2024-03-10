317da5f59033336014ec372738efa72f2f401685
mst-mkt <s1f102303057@iniad.org>
2024-03-11 02:03:16 +0900

    fix: adjust text size of textarea

--- components/common/TextArea.tsx
+++ components/common/TextArea.tsx
@@ -4,7 +4,7 @@ export const TextArea = (textAreaProps: JSX.IntrinsicElements["textarea"]) => {
   return (
     <textarea
       {...textAreaProps}
-      class="w-full min-h-32 resize-none p-2 text-xs border rounded-lg outline-0 ring-offset-2 focus-visible:ring-2"
+      class="w-full min-h-32 resize-none p-2 text-sm border rounded-lg outline-0 ring-offset-2 focus-visible:ring-2"
     />
   );
 };
