eff6248e09a9eac01447391d9cca66b333f8b19b
mst-mkt <s1f102303057@iniad.org>
2024-03-15 06:46:05 +0900

    add: lang attr for html element

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -3,7 +3,7 @@ import { Header } from "../components/layout/Header.tsx";
 
 export default function App({ Component }: PageProps) {
   return (
-    <html>
+    <html lang="ja">
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
