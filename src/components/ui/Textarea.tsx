18f3870768a371cc3da35524be8f1c0dd3647c4b
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-08-05 18:35:46 +0900

    fix(styles): hidden x overflow of textarea

--- src/components/ui/Textarea.tsx
+++ src/components/ui/Textarea.tsx
@@ -10,7 +10,7 @@ export const Textarea = (props: TextareaProps) => (
     className={twMerge(
       props.className,
       clsx(
-        'min-h-32 w-full resize-y rounded-lg border border-background-100 bg-background-50 p-2 text-sm outline-0',
+        'min-h-32 w-full resize-y overflow-x-hidden overflow-y-scroll rounded-lg border border-background-100 bg-background-50 p-2 text-sm outline-0',
         'scrollbar-thin scrollbar-thumb-background-200 scrollbar-thumb-rounded-full scrollbar-track-transparent',
         'focus-visible:ring-2 focus-visible:ring-accent focus-visible:ring-offset-2 focus-visible:ring-offset-background',
       ),
