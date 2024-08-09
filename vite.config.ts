1a0f7ca25ffd203c9f74b314f6c1bafdb34cb50a
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-08-10 07:04:45 +0900

    chore(vite): add config for cite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   define: {
@@ -8,6 +9,41 @@ export default defineConfig({
   },
   plugins: [
     react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['/favicon.svg', '/apple-touch-icon.png'],
+      injectRegister: 'auto',
+      manifest: {
+        name: 'tools',
+        short_name: 'tools',
+        description: 'A collection of various tools for myself',
+        theme_color: '#0087ff',
+        icons: [
+          {
+            src: 'pwa_192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa_512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa_512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'pwa_512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    }),
     TanStackRouterVite({
       routesDirectory: 'src/routes',
       generatedRouteTree: 'src/lib/tanstack-router/routeTree.gen.ts',
