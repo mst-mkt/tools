9209121c514876a7f62bfd05e343cd23e1b60389
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-09-11 00:56:56 +0900

    fix(math/radix): adjust styles

--- src/routes/_layout/math/radix.tsx
+++ src/routes/_layout/math/radix.tsx
@@ -12,8 +12,8 @@ import { copy } from '../../../utils/clipboard/copy'
 
 const searchParamsValidator = z.object({
   number: z.string().optional(),
-  fromRadix: z.number().int().min(2).catch(10),
-  toRadix: z.number().int().min(2).catch(10),
+  fromRadix: z.number().int().min(2).optional(),
+  toRadix: z.number().int().min(2).optional(),
 })
 
 export const Route = createFileRoute('/_layout/math/radix')({
@@ -37,7 +37,7 @@ const Radix = () => {
   } = Route.useSearch()
   const [number, onSetNumber] = useInputState(initialNumber ?? '0')
   const [fromRadix, onSetFromRadix, setFromRadix] = useInputState(initialFromRadix ?? 10)
-  const [toRadix, onSetToRadix, onToRadix] = useInputState(initialToRadix ?? 10)
+  const [toRadix, onSetToRadix, onToRadix] = useInputState(initialToRadix ?? 16)
   const { copyLink } = useCopyLink(Route.id)
 
   const convertedNumber = useMemo(
@@ -55,35 +55,49 @@ const Radix = () => {
       <Head title="Radix Converter" />
       <div className="flex flex-col gap-y-8">
         <h1 className="font-bold text-lg">基数変換</h1>
-        <Textarea
-          value={number}
-          onChange={onSetNumber}
-          placeholder="変換する数値を入力"
-          aria-label="変換する数値を入力"
-        />
-        <div className="justify- flex gap-x-4">
-          <NumberInput
-            value={fromRadix}
-            onChange={onSetFromRadix}
-            min={1}
-            max={36}
-            aria-label="変換元の基数を数値で入力"
-          />
-          <IconButton icon={IconArrowsRightLeft} label="Reverse" onClick={handlerReverse} />
-          <NumberInput
-            value={toRadix}
-            onChange={onSetToRadix}
-            min={1}
-            max={36}
-            aria-label="変換先の基数を数値で入力"
+        <div className="flex flex-col gap-y-2">
+          <div className="flex items-center justify-between gap-x-2">
+            <h2 className="grow font-bold">変換前</h2>
+            <NumberInput
+              value={fromRadix}
+              onChange={onSetFromRadix}
+              min={1}
+              max={36}
+              aria-label="変換元の基数を数値で入力"
+              className="w-16 grow-0"
+            />
+            <span>進数</span>
+          </div>
+          <Textarea
+            value={number}
+            onChange={onSetNumber}
+            placeholder="変換する数値を入力"
+            aria-label="変換する数値を入力"
           />
         </div>
-        <Textarea
-          value={convertedNumber}
-          readOnly={true}
-          placeholder="変換された数値"
-          aria-label="変換された数値の表示"
-        />
+        <div className="justify- flex gap-x-4">
+          <IconButton icon={IconArrowsRightLeft} label="基数を交換" onClick={handlerReverse} />
+        </div>
+        <div className="flex flex-col gap-y-2">
+          <div className="flex items-center justify-between gap-x-2">
+            <h2 className="grow font-bold">変換後</h2>
+            <NumberInput
+              value={toRadix}
+              onChange={onSetToRadix}
+              min={1}
+              max={36}
+              aria-label="変換先の基数を数値で入力"
+              className="w-16 grow-0"
+            />
+            <span>進数</span>
+          </div>
+          <Textarea
+            value={convertedNumber}
+            readOnly={true}
+            placeholder="変換された数値"
+            aria-label="変換された数値の表示"
+          />
+        </div>
         <div className="flex gap-x-2">
           <IconButton
             onClick={() => copyLink({ number, fromRadix, toRadix })}
