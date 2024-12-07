a998fad82b03b2c54bd16be58c1b1de70dbc1b91
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-12-08 06:58:55 +0900

    chore: include file starts with `.`

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,7 @@ import type { Config } from 'tailwindcss'
 import animate from 'tailwindcss-animate'
 
 const config = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', './src/**/.*.{js,ts,jsx,tsx}'],
   darkMode: ['class'],
   theme: {
     extend: {
