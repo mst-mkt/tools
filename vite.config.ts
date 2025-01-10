f8f249451918ffb3660c7eeddd65276652ee2ef3
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2025-01-11 06:05:48 +0900

    add: config for pwa

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 import mkcert from "vite-plugin-mkcert"
 import tailwindcss from '@tailwindcss/vite';
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   define: {
@@ -30,5 +31,43 @@ export default defineConfig({
       routeTreeFileFooter: [],
     }),
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['/favicon.svg', '/apple-touch-icon.png'],
+      injectRegister: 'auto',
+      manifest: {
+        name: 'tools',
+        short_name: 'tools',
+        description: 'A collection of various tools for myself',
+        theme_color: '#0087ff',
+        icons: [
+          {
+            src: 'pwa_192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa_512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa_512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'pwa_512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      },
+      workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+      }
+    }),
   ],
 })
