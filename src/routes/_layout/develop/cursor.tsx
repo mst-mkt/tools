d16a2c48a589bc96e24fec1a3bb7a1a4ed195f59
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2025-02-05 14:09:11 +0900

    feat: develop/cursor

--- src/routes/_layout/develop/cursor.tsx
+++ src/routes/_layout/develop/cursor.tsx
@@ -1,9 +1,25 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { object, optional, parse, picklist } from 'valibot'
+import { Head } from '../../../components/shared/Head'
+import { CURSORS } from '../../../features/develop/cursor/constants'
+import { Cursor } from '../../../features/develop/cursor/page'
+
+const searchParamsSchema = object({
+  cursor: optional(picklist(CURSORS)),
+})
 
 export const Route = createFileRoute('/_layout/develop/cursor')({
   component: RouteComponent,
+  validateSearch: (search) => parse(searchParamsSchema, search),
 })
 
 function RouteComponent() {
-  return <div>Hello "/_layout/develop/cursor"!</div>
+  const { cursor } = Route.useSearch()
+
+  return (
+    <>
+      <Head title="カーソル スタイルプレビュー" />
+      <Cursor initialSelectedCursor={cursor ?? 'auto'} />
+    </>
+  )
 }
