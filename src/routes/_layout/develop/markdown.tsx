7664852abc6b2b91be83c5af242dd11fec133a9f
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2025-02-05 15:13:24 +0900

    feat: develop/markdown

--- src/routes/_layout/develop/markdown.tsx
+++ src/routes/_layout/develop/markdown.tsx
@@ -1,9 +1,24 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { object, optional, parse, string } from 'valibot'
+import { Head } from '../../../components/shared/Head'
+import { Markdown } from '../../../features/develop/markdown/page'
+
+const searchParamsSchema = object({
+  markdown: optional(string()),
+})
 
 export const Route = createFileRoute('/_layout/develop/markdown')({
+  validateSearch: (search) => parse(searchParamsSchema, search),
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/_layout/develop/markdown"!</div>
+  const { markdown } = Route.useSearch()
+
+  return (
+    <>
+      <Head title="Markdown プレビュー" />
+      <Markdown initialMarkdown={markdown ?? ''} />
+    </>
+  )
 }
